a78caac2cf6f274f1047970d3044aac93b72fd70
Mohammed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2021-03-30 19:26:44 +0200

    Update 999B - Reversing Encryption.cpp

--- Codeforces/999B - Reversing Encryption.cpp
+++ Codeforces/999B - Reversing Encryption.cpp
@@ -4,34 +4,16 @@ using namespace std;
 
 int main()
 {
-	
 	string x;
 	int N;
 	cin >> N;
 	cin >> x;
-
-
 	for (int i = 1; i<= N; i++)
 	{
-	
 		if (N % i == 0)
 		{
 			reverse(x.begin(), x.begin() + i);
-
 		}
-
 	}
-	
 	cout << x << endl;
-	
-	
-	
-	
-	
-	
-
-
-
-
-
 }
