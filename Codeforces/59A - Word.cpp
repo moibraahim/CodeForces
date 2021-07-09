d631f3460d3de3d037e81f88525cd25af633f2af
Mohammed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2021-07-09 21:39:02 +0200

    Update 59A - Word.cpp

--- Codeforces/59A - Word.cpp
+++ Codeforces/59A - Word.cpp
@@ -1,62 +1,31 @@
 #include <iostream>
 using namespace std;
-
+ 
 int main()
 {
-
-	// if L  == U then all Lower
-	string word;
-	cin >> word;
-
-	int Lcount = 0;
-	int Ucount = 0;
-	for (int i = 0; i < word.length(); i++)
+ 
+	int n;
+	long long int  x[100000];
+	cin >> n;
+ 
+ 
+	for (int i = 0; i < n; i++)
 	{
-		if (word[i] >= 97 && word[i] <= 122)
+		cin >> x[i];
+	}
+ 
+	int count = 1;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (x[i] != x[i + 1])
 		{
-			Lcount++;
-		}
-		if (word[i] >= 65 && word[i] <= 92)
-		{
-			Ucount++;
+			count++;
 		}
 	}
-
-	if (Ucount == Lcount)
-	{
-		// Converting All to Lower
-		for (int i = 0; i < word.length(); i++)
-		{
-			if (word[i] >= 65 && word[i] <= 92)
-			{
-				word[i] = word[i] + 32;
-			}
-		}
-	}
-	if (Ucount > Lcount)
-	{
-		for (int i = 0; i < word.length(); i++)
-		{
-			if (word[i] >= 97 && word[i] <= 122)
-			{
-				word[i] = word[i] - 32;
-			}
-		}
-	}
-	if (Lcount > Ucount)
-	{
-		for (int i = 0; i < word.length(); i++)
-		{
-			if (word[i] >= 65 && word[i] <= 92)
-			{
-				word[i] = word[i] + 32;
-			}
-		}
-	}
-
-	cout << word << endl;
-
-
-
-
+ 
+	cout << count << endl;
+ 
+ 
+ 
+ 
 }
