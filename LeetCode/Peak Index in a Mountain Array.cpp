240c6e03b8cfcbe747636fced72fe1e739484fc0
Mohammed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2021-09-28 06:10:40 +0200

    Create Peak Index in a Mountain Array.cpp

---
+++ LeetCode/Peak Index in a Mountain Array.cpp
@@ -0,0 +1,21 @@
+// Could be solved in O(log n) Time Complexity using binray search
+
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) 
+    {
+        int max = LONG_MIN;
+        int MaxPos;
+            
+        for (int i=0; i<arr.size(); i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                MaxPos = i;
+            }
+        }
+        return MaxPos;
+    }
+};
